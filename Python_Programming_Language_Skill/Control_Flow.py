784e1e6046e9e9c4c21a9d76ee47e57fbcf253a4
Mandadi Mahendhar Reddy <mandadimahendharreddy8523@gmail.com>
2025-06-23 15:27:11 +0530

    python syntax rules, variables, data type introduction, conditional statemenrt(if, if-else, elif), operators

---
+++ Python_Programming_Language_Skill/Control_Flow.py
@@ -0,0 +1,18 @@
+# 1. Conditional Statements (if-else)
+# Real-Time Example 1: Login Validation
+# Let's say you're creating a login system where the program checks the user's credentials.
+username = "admin"
+password = "1234"
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
+# Real-Time Example 2: Discount Eligibility
+# In an e-commerce site, a user might receive a discount if their total amount is more than $100.
+total_amount = 120
+if total_amount > 100:
+    print("You get a 10% discount!")
+else:
+    print("No discount available.")
+
+
