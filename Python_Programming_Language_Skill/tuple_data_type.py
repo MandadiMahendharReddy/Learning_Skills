35ec674b67f154fc284f2be4f33d42f4b5d9eb39
Mandadi Mahendhar Reddy <mandadimahendharreddy8523@gmail.com>
2025-06-25 20:23:31 +0530

    1_Python: Solved Tasks in string and tuple using all methods

--- Python_Programming_Language_Skill/tuple_data_type.py
+++ Python_Programming_Language_Skill/tuple_data_type.py
@@ -20,3 +20,40 @@ def get_user():
     return ("Mahendhar", 27)
 name, age = get_user()
 print(name)  # Mahendhar
+
+
+# Challenge
+# 🛠️ TASKS (Use tuple methods and features):
+# Print the student’s full record neatly.
+# Access the name and branch using indexing.
+# Convert tuple to list, update the branch to "AI & DS", then convert it back to tuple.
+# Add a new field to tuple: "Passed" (Note: Tuples are immutable, so think smartly).
+# Count how many times 2025 appears in the tuple.
+# Find the index of "Computer Science" in the original tuple.
+# Slice the tuple to only show the first 3 fields.
+# Print the length of the final tuple.
+# Loop through all items and print one by one.
+# Use type() to confirm it’s a tuple.
+
+def student_data(student):
+    print(student)
+    print(student[0], student[2])
+    student = list(student)
+    student[2] = "AI & DS"
+    student=tuple(student)
+    print(student)
+    student=list(student)
+    student.append("Passed")
+    student=tuple(student)
+    print(student)
+    print(student.count(2025))
+    student1 = ('Mahendhar', 101, 'Computer Science', 2025)
+    print(student1.index('Computer Science'))
+    print(student1[0:3])
+    print(len(student))
+    for i in student:
+        print(i)
+    print(type(student))
+
+student = ("Mahendhar", 101, "Computer Science", 2025)
+student_data(student)
