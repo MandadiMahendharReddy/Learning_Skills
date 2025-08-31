b11994fd7878a5e4f599bb7778fa2b6b26e6e164
Mandadi Mahendhar Reddy <mandadimahendharreddy8523@gmail.com>
2025-08-31 19:15:46 +0530

    2_Django: Blog project from admin

---
+++ Django_web_framework_skill/myproject/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')  # latest first
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
